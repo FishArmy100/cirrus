6095921344b76944a1fd443fb18a383522804305
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2025-04-07 19:24:23 -0400

    bump

--- src/typed_ast/mod.rs
+++ src/typed_ast/mod.rs
@@ -1,6 +1,18 @@
+pub mod builtins;
+
 use std::collections::HashMap;
 
 use uuid::Uuid;
+
+pub enum TypeError
+{
+    NoGenerics
+    {
+        type_id: TypeRef,
+    }
+}
+
+pub type TypeInfoResult = Result<TypeInfo, TypeError>;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum UnaryOpType
@@ -9,6 +21,7 @@ pub enum UnaryOpType
     Negate, // -
 }
 
+#[derive(Debug, Clone)]
 pub struct UnaryOp 
 {
     pub operated: TypeRef,
@@ -47,6 +60,7 @@ pub enum BinaryOpType
     Or,
 }
 
+#[derive(Debug, Clone)]
 pub struct BinaryOp 
 {
     pub left: TypeRef,
@@ -67,8 +81,20 @@ impl BinaryOp
             result: type_id
         }
     }
+
+    pub fn isosceles(args: TypeRef, op: BinaryOpType, ret: TypeRef) -> Self 
+    {
+        Self 
+        { 
+            left: args.clone(), 
+            right: args.clone(), 
+            result: ret, 
+            op
+        }
+    }
 }
 
+#[derive(Debug, Clone)]
 pub struct IndexOp
 {
     pub indexed: TypeRef,
@@ -76,6 +102,7 @@ pub struct IndexOp
     pub result: TypeRef,
 }
 
+#[derive(Debug, Clone)]
 pub struct CallOp
 {
     pub called: TypeInfo,
@@ -83,6 +110,14 @@ pub struct CallOp
     pub result: TypeInfo,
 }
 
+#[derive(Debug, Clone)]
+pub struct CastOp
+{
+    pub casted: TypeRef,
+    pub caster: TypeRef,
+}
+
+#[derive(Debug, Clone)]
 pub struct Member
 {
     pub name: String,
@@ -92,12 +127,14 @@ pub struct Member
     pub is_type_def: bool, // type T = Int
 }
 
+#[derive(Debug, Clone)]
 pub struct Variant 
 {
     pub name: String,
     pub type_info: TypeRef
 }
 
+#[derive(Debug, Clone)]
 pub struct TypeInfo 
 {
     pub id: TypeRef,
@@ -109,17 +146,18 @@ pub struct TypeInfo
     pub call_ops: Vec<CallOp>,
     pub members: Vec<Member>, // functions, types, 
     pub variants: Vec<Variant>,
+    pub cast_ops: Vec<CastOp>,
 
     pub is_variant: bool,
 }
 
 impl TypeInfo
 {
-    pub fn new_primary(name: impl ToString) -> Self
+    pub fn new_primary(id: TypeRef, name: impl ToString) -> Self
     {
         Self 
         {
-            id: uuid::Uuid::new_v4(),
+            id,
             name: name.to_string(),
             unary_ops: HashMap::new(),
             binary_ops: HashMap::new(),
@@ -127,44 +165,57 @@ impl TypeInfo
             call_ops: vec![],
             members: vec![],
             variants: vec![],
+            cast_ops: vec![],
             is_variant: false,
         }
     }
 
     pub fn insert_uniform_binary_ops(&mut self, ops: &[BinaryOpType]) -> &mut Self
     {
+        ops.iter().for_each(|op| {
+            self.binary_ops.insert(*op, BinaryOp::uniform(self.id.clone(), *op));
+        });
         self
     }
 
-    pub fn insert_uniform_unary_ops(&mut self, ops: &[UnaryOpType]) -> &mut Self
+    pub fn insert_uniform_unary(&mut self, op: UnaryOpType) -> &mut Self
     {
+        self.unary_ops.insert(op, UnaryOp::uniform(self.id.clone(), op));
+        self
+    }
+
+    pub fn insert_casts(&mut self, casters: &[TypeRef]) -> &mut Self
+    {
+        casters.iter().for_each(|c| {
+            self.cast_ops.push(CastOp { casted: self.id.clone(), caster: c.clone() });
+        });
+
+        self
+    }
+
+    pub fn insert_isosceles_binary_ops(&mut self, ops: &[BinaryOpType], ret: TypeRef) -> &mut Self 
+    {
+        ops.iter().for_each(|op| {
+            self.binary_ops.insert(*op, BinaryOp::isosceles(self.id.clone(), *op, ret));
+        });
+
         self
     }
 }
 
 pub type TypeRef = Uuid;
 
-pub struct TypeGenerator(Box<dyn Fn(Vec<TypeInfo>) -> TypeInfo>);
+pub struct TypeGenerator(Box<dyn Fn(Vec<TypeInfo>) -> TypeInfoResult>);
 
 impl TypeGenerator
 {
-    pub fn gen(&self, types: Vec<TypeInfo>) -> TypeInfo
+    pub fn gen(&self, types: Vec<TypeInfo>) -> TypeInfoResult
     {
         self.0(types)
     }
 }
 
-fn build_builtins() -> HashMap<Uuid, TypeGenerator>
-{
-    let mut map = HashMap::new();
 
-    let mut int_type = TypeInfo::new_primary("Int");
-    let int_id = int_type.id.clone();
-
-    int_type.binary_ops.insert(BinaryOpType::Plus, BinaryOp { left: int_id.clone(), right: (), result: (), op: () })
-
-    map
-}
 
 pub struct TypedAst
 {
